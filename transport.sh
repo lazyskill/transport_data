1e3fb177b200f5cc758eea1994990847f4dc086b
jun <jun@cy.com>
2019-05-10 17:38:53 +0000

    update

--- transport.sh
+++ transport.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 cp ~/mserver/EntranceServer/huginn_*.csv ./
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+ABORT() {
+    [ "$1" ] && echo -e "[${red}ERROR${plain}]: $*"
+    exit 1
+}
+
 git add .
-echo $?
+[ $? -ne 0] && ABORT "add error"
 git commit -m "update" .
-echo $?
+[ $? -ne 0] && ABORT "commit error"
 git push
-echo $?
+[ $? -ne 0] && ABORT "push error"
