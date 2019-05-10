cbf28946dec62dedb35b00d996ef249465dafcaa
jun <jun@cy.com>
2019-05-11 02:31:01 +0800

    update

--- transport.sh
+++ transport.sh
@@ -6,7 +6,7 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 ABORT() {
-    [ "$1" ] && echo -e "[${red}ERROR${plain}]: $*"
+    #[ "$1" ] && echo -e "[${red}ERROR${plain}]: $*"
     exit 1
 }
 
